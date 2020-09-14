4264bf9c1c8a73d835c1ca02c8840ee394354b2d
martti <martti@scr.ee>
2020-09-14 17:11:56 +0300

    added slider

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <TopHeader />
-    <item />
+    <Item />
   </div>
 </template>
 
 <script>
-import item from './components/item'
+import Item from './components/item'
 import TopHeader from './components/top-header.vue'
 
 export default {
   name: 'App',
   components: {
     TopHeader,
-    item
+    Item
   },
   methods: {
     
@@ -38,74 +38,6 @@ html {
     font-weight: 500;
     color: #f3f3f3;
     
-}
-
-h1,
-h3 {
-    text-align: center;
-    width: 100%;
-    margin-top: 15px;
-}
-
-h3 {
-    font-weight: 500;
-    letter-spacing: 1px;
-}
-
-.input {
-    text-align: center;
-    margin-top: 10px;
-}
-
-.input input {
-    height: 25px;
-    font-size: 20px;
-    width: 140px;
-    text-align: center;
-    background-color: #15202b;
-    border: solid 1px;
-    color: #ffffff;
-    outline: none;
-}
-
-.radioButtons {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-}
-.radioButtons div {
-    display: flex;
-    justify-content: start;
-    margin: 10px;
-}
-
-.radioButtons div label {
-    margin-left: 5px;
-}
-
-.radioButtons input {
-    margin-top: 2px;
-}
-
-.btn1 {
-    display: block;
-    width: 120px;
-    height: 30px;
-    margin: 10px auto;
-    border:solid rgb(255, 255, 255) 1px;
-    color: #ffffff;
-    background: #15202b;
-    transition: background 0.3s;
-    font-size: 95%;
-    outline: none;
-    font-weight: 500;
-}
-
-.btn1:hover,
-.btn1:active {
-    background: #f3f3f3;
-    color: #15202b;
-    cursor: pointer;
 }
 
 </style>
--- src/components/top-header.vue
+++ src/components/top-header.vue
@@ -3,69 +3,101 @@
     <h1>SKICALC</h1>
     <h3>enter your fis-height below</h3>
     <form>
-          <div class="input">
-            <input 
-              v-model="userData.height" 
-              type="number" 
-              placeholder = "Your height" 
-              @keypress.enter.prevent ="calculateResults">
-          </div>
-          <div class="radioButtons">
-            <div>
-              <input 
-                type="radio" 
-                name="results" 
-                value="10" 
-                v-model="userData.amountOfResults">
-              <label for="10">10 results</label>
-            </div>
-            <div>
-              <input 
-                type="radio" 
-                name="results" 
-                value="15" 
-                v-model="userData.amountOfResults">
-              <label for="15">15 results</label>
-            </div>
-            <div>
-              <input 
-                type="radio" 
-                name="results" 
-                value="20" 
-                v-model="userData.amountOfResults">
-              <label for="20">20 results</label>
-            </div>
-          </div>
-          <div>
-            <button 
-              type="button" 
-              @click="calculateResults" 
-              class ="btn1"
-            >SUBMIT</button>	
-          </div>
-        </form>
+      <div class="input-container">
+        <input
+          class="height-input"
+          v-model="userData.height"
+          type="number"
+          placeholder="Your height"
+          @keypress.enter.prevent="calculateResults"
+        />
+      </div>
+      <div class="center-item">
+        <vue-slider v-model="userData.amountOfResults" :width="200" :max="25" :dotSize="16" />
+      </div>
+      <div>
+        <button type="button" @click="calculateResults" class="btn1">CALCULATE</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
 export default {
+  components: {
+    VueSlider,
+  },
   data() {
     return {
       userData: {
-        height: '',
-        amountOfResults: 15
-      }
-    }
+        height: "",
+        amountOfResults: 10,
+      },
+    };
   },
   methods: {
     calculateResults() {
-      this.$store.dispatch('calculateData', this.userData)
-    }
-  }
-
-}
+      this.$store.dispatch("calculateData", this.userData);
+    },
+  },
+};
 </script>
 
 <style scoped>
+.center-item {
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+}
+h1,
+h3 {
+  text-align: center;
+  width: 100%;
+  margin-top: 15px;
+}
 
+h3 {
+  font-weight: 500;
+  letter-spacing: 1px;
+}
+.input-container {
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.height-input {
+  height: 42px;
+  font-size: 20px;
+  width: auto;
+  text-align: center;
+  background-color: #15202b;
+  border: solid 1px;
+  color: #ffffff;
+  outline: none;
+}
+
+.btn1 {
+  display: block;
+  width: 120px;
+  height: 30px;
+  margin: 10px auto;
+  border: solid rgb(255, 255, 255) 1px;
+  color: #ffffff;
+  background: #15202b;
+  transition: background 0.3s;
+  font-size: 95%;
+  outline: none;
+  font-weight: 500;
+}
+
+.btn1:hover,
+.btn1:active {
+  background: #f3f3f3;
+  color: #15202b;
+  cursor: pointer;
+}
 </style>
